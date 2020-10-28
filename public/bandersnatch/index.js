51169d0b7dd4dc330cb29a2aa923296ea17cff4a
tiagobani <tiago.bani@yahoo.com.br>
2020-10-28 20:50:55 -0300

    [feat]: add videoPlayer and videoComponent

--- public/bandersnatch/index.js
+++ public/bandersnatch/index.js
@@ -1,13 +1,21 @@
-async function main() {
-    const player = videojs('vid');
-    const ModalDialog = videojs.getComponent('ModalDialog');
-    const modal = new ModalDialog(player, {
-        temporary: false, 
-        closeable: true
-    });
+const MANIFEST_URL = 'manifest.json'
+const localHost = ['localhost', '127.0.0.1']
 
-    player.addChild(modal);
+async function main () {
+  const isLocal = !!~localHost.indexOf(window.location.hostname)
+  console.log({ isLocal })
+  const manifestJSON = await (await fetch(MANIFEST_URL)).json()
 
+  const host = isLocal ? manifestJSON.localHost : manifestJSON.productionHost
+  const videoComponent = new VideoComponent()
+
+  const network = new Network({ host })
+  const videoPlayer = new VideoPlayer({
+    manifestJSON, network
+  })
+
+  videoPlayer.inicializeCodec()
+  videoComponent.inicializePlayer()
 }
 
 window.onload = main
